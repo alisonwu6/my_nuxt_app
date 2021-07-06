ca6ed06965f9b6db52e6263a26e5f961c0a7c5a2
Alison Wu <alison.wu23@gmail.com>
2021-07-06 22:13:26 +0800

    add: pages/admin & UI components

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,18 +2,21 @@
   <section class="post-list">
     <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="Hello there!"
       previewText="This my first post!"
     />
     <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="Hello there - the second time!"
       previewText="This my second post!"
     />
     <PostPreview
       id="3"
+      :is-admin="isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="Hi!"
       previewText="This my third post!"
@@ -26,6 +29,12 @@ import PostPreview from '@/components/Posts/PostPreview.vue';
 export default {
   components: {
     PostPreview,
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
 };
 </script>
