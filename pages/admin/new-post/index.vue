df8ea0cdd751d0ddf418c035f72425ab362b97ed
Alison Wu <alison.wu23@gmail.com>
2021-07-07 20:30:12 +0800

    fix: updatedDate is added

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -18,7 +18,10 @@ export default {
   methods: {
     onSubmitted(postData) {
       axios
-        .post('https://nuxt-porfolio-app-default-rtdb.firebaseio.com/posts.json', postData)
+        .post('https://nuxt-porfolio-app-default-rtdb.firebaseio.com/posts.json', {
+          ...postData,
+          updatedDate: new Date(),
+        })
         .then((result) => console.log(result))
         .catch((e) => console.log(e));
     },
