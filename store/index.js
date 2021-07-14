dad15450a48b94e2ee8498dfc36fd0ac047e6077
Alison Wu <alison.wu23@gmail.com>
2021-07-14 11:31:01 +0800

    Adding the Logout Functionality

--- store/index.js
+++ store/index.js
@@ -109,11 +109,20 @@ const createStore = () => {
         }
 
         if (new Date().getTime() > Number.parseInt(expirationDate) || !token) {
-          vuexContext.commit('clearToken');
+          vuexContext.dispatch("logout");
           return;
         }
 
         vuexContext.commit("setToken", token);
+      },
+      logout(vuexContext) {
+        vuexContext.commit("clearToken");
+        Cookie.remove("jwt");
+        Cookie.remove("expirationDate");
+        // if (process.client) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("tokenExpiration");
+        // }
       }
     },
     getters: {
