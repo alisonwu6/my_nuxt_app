efe1bc7afd4ed3a77c8e711ae8c396d4cd184b28
Alison Wu <alison.wu23@gmail.com>
2021-07-14 10:56:02 +0800

    fixing logout timer

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,5 +1,5 @@
 export default function (context) {
-  console.log("[Middleware] check-auth");
-  // context.req should be null on client-side.
+  console.log("[Middleware] check-auth", "initAuth");
   context.store.dispatch("initAuth", context.req);
+  // context.req should be null on client-side.
 }
